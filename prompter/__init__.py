5cb50076870cf6905d4de3509a36e43507bda955
Dmitriy Shabat <mitya.shabat@gmail.com>
2020-04-12 19:46:01 +0300

    Corrected prompter

--- prompter/__init__.py
+++ prompter/__init__.py
@@ -2,19 +2,19 @@ class Prompter:
 
     """ My Cmd class for preprocessing the Python interpreter"""
 
-    DEFAULT_PROMPT = ">>> "
-    DEFAULT_INTRO = "Welcome to Prompter!\n"
-
-    def __init__(self, stdin = None, stdout = None, prompt = None, intro = None):
+    def __init__(self, stdin = None, stdout = None, prompt = None, intro = None, outro = None):
         self.stdin = stdin
         self.stdout = stdout
-        self.prompt = prompt if prompt is not None else DEFAULT_PROMPT
-        self.intro = intro if intro is not None else DEFAULT_INTRO
+        self.prompt = prompt if prompt is not None else ">>> "
+        self.intro = intro if intro is not None else "Welcome to Prompter!\n"
+        self.outro = outro if outro is not None else ""
         
     def readline(self):
         """ Read a line from specified stdin """
-
-        return self.stdin.readline()
+        if self.stdin is not None:
+            return self.stdin.readline()
+        else:
+            return input()
     def print(self, *args, **kwargs):
         """ Print to specified stdout """
 
@@ -29,16 +29,23 @@ class Prompter:
     """ Methods for subclass implementation """
     def pre_prompt(self):
         pass
-    def on_prompt(self, line):
-        pass
+    def on_eof(self) -> bool:
+        return False
+    def on_prompt(self, line: str) -> bool:
+        return True
     def post_prompt(self):
         pass
 
     def cmdloop(self):
+        self.print(self.intro)
         while True:
             self.pre_prompt()
-            line = self.prompt_input()
-            exit_flag = self.on_prompt(line)
+            try:
+                line = self.prompt_input()
+                ok_flag = self.on_prompt(line)
+            except EOFError:
+                ok_flag = self.on_eof()
             self.post_prompt()
-            if exit_flag:
+            if not ok_flag:
+                self.print(self.outro)
                 break
