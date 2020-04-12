49186930260e5be324dba4145b3ecd7a4589a2be
Dmitriy Shabat <mitya.shabat@gmail.com>
2020-04-12 23:41:51 +0300

    Added tests and functionality for variable machine

--- tests/test_variable_machine.py
+++ tests/test_variable_machine.py
@@ -10,7 +10,7 @@ class TestVariableMachine(unittest.TestCase):
     def test_translation(self):
         self.assertEqual(
             self.machine.names_to_dict("a = b + c", "d"), 
-            "d[a] = d[b] + d[c]"
+            "d[\"a\"] = d[\"b\"] + d[\"c\"]"
         )
     def test_identifier(self):
         self.assertTrue(
@@ -19,4 +19,11 @@ class TestVariableMachine(unittest.TestCase):
         self.assertFalse(
             VariableMachine.is_identifier("abc-dce+afs")
         )
+    def test_assign(self):
+        self.machine.assign("a", "1")
+        self.assertEqual(self.machine.get("a"), 1)
+
+        self.machine.assign("a", "1 + 1 + 1 + 2 * 0")
+        self.machine.assign("a", "a * 2 + 2 ** a-1")
+        self.assertEqual(self.machine.get("a"), 3 * 2 + 2 ** 3 - 1)
     
--- variable_machine/__init__.py
+++ variable_machine/__init__.py
@@ -1,4 +1,5 @@
 import re
+from typing import Any
 
 class VariableMachine:
 
@@ -10,17 +11,26 @@ class VariableMachine:
     variable_regex = r"([a-zA-Z_][a-zA-Z_0-9]*)"
 
     @classmethod
-    def is_identifier(cls, name):
+    def is_identifier(cls, name: str) -> bool:
         """ Checks whether the given name is suitable for a variable name """
-        return re.match(name, "$" + cls.variable_regex + "^")
 
-    def names_to_dict(self, expression: str, dict_name: str) -> str:
+        return re.match("^" + cls.variable_regex + "$", name) is not None
+    @classmethod
+    def names_to_dict(cls, expression: str, dict_name: str) -> str:
         """ Replace variable names in expression with references to values in dict """
-        self.
-        return re.sub(self.variable_regex, r"%s[\1]" % dict_name, expression)
 
-    def assign(self, name, expression):
+        return re.sub(cls.variable_regex, "%s[\"\\1\"]" % dict_name, expression)
+
+    def eval(self, expression: str) -> Any:
+        """ Evaluate the expression using the variables in this machine """
+
+        return eval(VariableMachine.names_to_dict(expression, "self.var"))
+    def assign(self, name: str, expression: str) -> None:
         """ Assign result of expression to a variable with a given name """
-        raise NotImplementedError
 
+        self.var[name] = self.eval(expression)
+    def get(self, name: str) -> Any:
+        """ Get stored variable """
+
+        return self.var[name]
         
