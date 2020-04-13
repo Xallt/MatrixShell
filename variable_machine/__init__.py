ba6482816e67998643e1c17cfcb3514e3092130b
Dmitry <mitya.shabat@gmail.com>
2020-04-13 13:54:42 +0300

    Corrected regex

--- variable_machine/__init__.py
+++ variable_machine/__init__.py
@@ -8,8 +8,6 @@ class VariableMachine:
     def __init__(self):
         self.var = dict()
 
-    variable_regex = r"([a-zA-Z_][a-zA-Z_0-9]*)"
-
     @classmethod
     def is_identifier(cls, name: str) -> bool:
         """ Checks whether the given name is suitable for a variable name """
@@ -21,15 +19,16 @@ class VariableMachine:
         """ Replace variable names in expression with references to values in dict """
         if len(names) == 0:
             return expression
-        return re.sub(r"(%s)" % '|'.join(names), "%s[\"\\1\"]" % dict_name, expression)
+        pattern = r"(?!<=[a-zA-Z_0-9])(%s)(?!=[a-zA-Z_0-9])" % '|'.join(names)
+        return re.sub(pattern, "%s[\"\\1\"]" % dict_name, expression)
 
     def names(self):
         """ Return list of assigned variable names """
         return self.var.keys()
     def eval(self, expression: str) -> Any:
         """ Evaluate the expression using the variables in this machine """
-
-        return eval(VariableMachine.names_to_dict(expression, self.names(), "self.var"))
+        processed_expression = VariableMachine.names_to_dict(expression, self.names(), "self.var")
+        return eval(processed_expression)
     def assign(self, name: str, expression: str) -> None:
         """ Assign result of expression to a variable with a given name """
 
